05668f634c9d7adb7e192d2cb7d545c09c7e56c9
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-03-21 14:22:59 -0400

    fix

--- FactorialAllisonC/FactorialForm.Designer.cs
+++ FactorialAllisonC/FactorialForm.Designer.cs
@@ -28,43 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblget = new System.Windows.Forms.Label();
+            this.lblanswer = new System.Windows.Forms.Label();
             this.mnuMenu = new System.Windows.Forms.MenuStrip();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
             this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtUserNumber = new System.Windows.Forms.TextBox();
+            this.lstIncrements = new System.Windows.Forms.ListBox();
             this.mnuMenu.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnStart
             // 
-            this.button1.Location = new System.Drawing.Point(88, 119);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnStart.Font = new System.Drawing.Font("Gentium Basic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(95, 85);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(69, 26);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
-            // label1
+            // lblget
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 67);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lblget.AutoSize = true;
+            this.lblget.Font = new System.Drawing.Font("Gentium Basic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblget.Location = new System.Drawing.Point(22, 45);
+            this.lblget.Name = "lblget";
+            this.lblget.Size = new System.Drawing.Size(115, 19);
+            this.lblget.TabIndex = 1;
+            this.lblget.Text = "Enter a Number:";
             // 
-            // label2
+            // lblanswer
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(107, 185);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.lblanswer.AutoSize = true;
+            this.lblanswer.Font = new System.Drawing.Font("Gentium Basic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblanswer.Location = new System.Drawing.Point(109, 224);
+            this.lblanswer.Name = "lblanswer";
+            this.lblanswer.Size = new System.Drawing.Size(0, 19);
+            this.lblanswer.TabIndex = 2;
             // 
             // mnuMenu
             // 
@@ -75,20 +78,6 @@
             this.mnuMenu.Size = new System.Drawing.Size(283, 24);
             this.mnuMenu.TabIndex = 3;
             this.mnuMenu.Text = "menuStrip1";
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(78, 27);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
-            this.progressBar1.TabIndex = 5;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(145, 64);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 6;
             // 
             // mniFile
             // 
@@ -101,9 +90,25 @@
             // mniExit
             // 
             this.mniExit.Name = "mniExit";
-            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Size = new System.Drawing.Size(92, 22);
             this.mniExit.Text = "Exit";
             this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // txtUserNumber
+            // 
+            this.txtUserNumber.Location = new System.Drawing.Point(162, 45);
+            this.txtUserNumber.Name = "txtUserNumber";
+            this.txtUserNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtUserNumber.TabIndex = 6;
+            // 
+            // lstIncrements
+            // 
+            this.lstIncrements.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lstIncrements.FormattingEnabled = true;
+            this.lstIncrements.Location = new System.Drawing.Point(72, 117);
+            this.lstIncrements.Name = "lstIncrements";
+            this.lstIncrements.Size = new System.Drawing.Size(120, 95);
+            this.lstIncrements.TabIndex = 7;
             // 
             // frmFactorial
             // 
@@ -111,11 +116,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Thistle;
             this.ClientSize = new System.Drawing.Size(283, 261);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstIncrements);
+            this.Controls.Add(this.txtUserNumber);
+            this.Controls.Add(this.lblanswer);
+            this.Controls.Add(this.lblget);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.mnuMenu);
             this.MainMenuStrip = this.mnuMenu;
             this.Name = "frmFactorial";
@@ -129,14 +134,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblget;
+        private System.Windows.Forms.Label lblanswer;
         private System.Windows.Forms.MenuStrip mnuMenu;
         private System.Windows.Forms.ToolStripMenuItem mniFile;
         private System.Windows.Forms.ToolStripMenuItem mniExit;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtUserNumber;
+        private System.Windows.Forms.ListBox lstIncrements;
     }
 }
 
--- FactorialAllisonC/FactorialForm.cs
+++ FactorialAllisonC/FactorialForm.cs
@@ -3,7 +3,7 @@
  * Created on: 20 march, 2018
  * Created for: ICS3U Programming
  * Daily Assignment – Day #20 - Factorial Do While
- * This program
+ * This program calculates the answer of the factorial of a given number
 */
 using System;
 using System.Collections.Generic;
@@ -19,9 +19,58 @@ namespace FactorialAllisonC
 {
     public partial class frmFactorial : Form
     {
+        //variables
+        double factorialAnswer, factorialNumber, Counter;
+
         public frmFactorial()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //set counter to 0
+            Counter = 0;
+
+            //set answer to one
+            factorialAnswer = 1;
+
+            //clear list
+            this.lstIncrements.Items.Clear();
+
+            //see if they input a number
+            try
+            {
+                factorialNumber = Convert.ToDouble(txtUserNumber.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a number.");
+            }
+
+            if (factorialNumber > 0)
+            {
+                do
+                {
+                    //increment counter by one
+                    Counter = Counter + 1;
+
+                    //show the counter number to show number of increments
+                    lstIncrements.Items.Add(Counter);
+
+                    //multiply the counter by the answer
+                    factorialAnswer = factorialAnswer * Counter;
+
+                } while (factorialNumber <= Counter);
+
+                //convert answer to string
+                lblanswer.Text = txtUserNumber.Text + "! = " + Convert.ToString(factorialAnswer);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a positive number.");
+            }
+
         }
 
         private void mniExit_Click(object sender, EventArgs e)
