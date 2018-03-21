85bc16e547349dec3b219e62f708c3b35851870d
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-03-21 13:54:10 -0400

    Initial commit

---
+++ FactorialAllisonC/FactorialForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 20 march, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #20 - Factorial Do While
+ * This program
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialAllisonC
+{
+    public partial class frmFactorial : Form
+    {
+        public frmFactorial()
+        {
+            InitializeComponent();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //close the program when clicked
+            this.Close();
+        }
+    }
+}
